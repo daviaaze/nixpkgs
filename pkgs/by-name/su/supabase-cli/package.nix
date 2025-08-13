6b72cc2d041db928b753115bd3f768b83a386b5e
Davi Azevedo <daviaaze@gmail.com>
2025-08-13 00:00:16 -0300

    supabase-cli 2.23.7 -> 2.34.3

--- pkgs/by-name/su/supabase-cli/package.nix
+++ pkgs/by-name/su/supabase-cli/package.nix
@@ -10,16 +10,16 @@
 
 buildGoModule rec {
   pname = "supabase-cli";
-  version = "2.23.7";
+  version = "2.34.3";
 
   src = fetchFromGitHub {
     owner = "supabase";
     repo = "cli";
     rev = "v${version}";
-    hash = "sha256-djzeqSaDTAK8WR3y7hpDk6NpuGlGLMxLvgSTH1IvlWw=";
+    hash = "sha256-CpDbHGrzsYudv5mrWwIGqB7sgcnmPK/wsc1AbIebdMI=";
   };
 
-  vendorHash = "sha256-sjF0NEea51CKPWuZZHsDVZDtQaYz/94qM5iYwPb3jNo=";
+  vendorHash = "sha256-PMAOTuQAA50bCnw6q/+2133cONUDiYkIv4x7ke4/G2c=";
 
   ldflags = [
     "-s"
@@ -27,12 +27,13 @@ buildGoModule rec {
     "-X=github.com/supabase/cli/internal/utils.Version=${version}"
   ];
 
+  subPackages = [ "." ];
+
   doCheck = false; # tests are trying to connect to localhost
 
   nativeBuildInputs = [ installShellFiles ];
 
   postInstall = ''
-    rm $out/bin/{docs,listdep}
     mv $out/bin/{cli,supabase}
 
     installShellCompletion --cmd supabase \
